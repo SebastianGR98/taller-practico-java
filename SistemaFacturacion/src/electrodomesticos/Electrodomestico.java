951c0b7897d13edc5107011312d7190c2f44b877
sebastiangrajales1998 <sebastiangrajales1998@gmail.com>
2021-02-11 15:44:51 -0500

    Se pasó el calculo del precio base a la superclase

--- SistemaFacturacion/src/electrodomesticos/Electrodomestico.java
+++ SistemaFacturacion/src/electrodomesticos/Electrodomestico.java
@@ -23,6 +23,30 @@ public abstract class Electrodomestico {
     }
 
     protected void calcularPrecio() {
+        // Precio base
+        this.precio = calcularPrecioConsumo() + calcularPrecioProcedencia();
+    }
 
+    private int calcularPrecioConsumo() {
+        // Precio de acuerdo al consumo
+        switch (this.consumo) {
+            case 'A':
+                return 450000;
+            case 'B':
+                return 350000;
+            case 'C':
+                return 250000;
+            default:
+                return 0;
+        }
+    }
+
+    private int calcularPrecioProcedencia() {
+        // Precio de acuerdo a la procedencia
+        if (this.procedencia.equals("Nacional")) {
+            return 250000;
+        } else {
+            return 350000;
+        }
     }
 }
