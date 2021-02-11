ccb5dcbcf754e0d6f3bc48a11e9cd04b24c8fbc4
sebastiangrajales1998 <sebastiangrajales1998@gmail.com>
2021-02-11 18:53:19 -0500

    Clase factura con funcionalidad de ingreso de electrodomesticos

---
+++ SistemaFacturacion/src/facturacion/Factura.java
@@ -0,0 +1,195 @@
+package facturacion;
+
+import electrodomesticos.Electrodomestico;
+import electrodomesticos.Nevera;
+import electrodomesticos.Televisor;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Factura {
+    private HashMap<Integer, Electrodomestico> electrodomesticos;
+    private int totalElectrodomesticos;
+    private int totalTelevisores;
+    private int totalNeveras;
+
+    public Factura() {
+        this.electrodomesticos = new HashMap<Integer, Electrodomestico>();
+        this.totalElectrodomesticos = 0;
+        this.totalTelevisores = 0;
+        this.totalNeveras = 0;
+        facturar();
+    }
+
+    private void facturar() {
+        Scanner entrada = new Scanner(System.in);
+        int numeroElectrodomesticos;
+
+        // El usuario ingresa cuantos electrodomesticos quiere facturar
+        while (true) {
+            try {
+                System.out.print("Ingrese el número de electrodomésticos que desea facturar: ");
+                numeroElectrodomesticos = entrada.nextInt();
+                break;
+            } catch (Exception e) {
+                System.out.println("Favor ingresar un número entero");
+                entrada.next();
+            }
+        }
+
+        ingresarElectrodomesticos(entrada, numeroElectrodomesticos);
+    }
+
+    private char ingresarConsumo(Scanner entrada) {
+        char consumo;
+
+        // El usuario ingresa consumo
+        while (true) {
+            try {
+                System.out.print("Ingrese consumo (A, B ó C): ");
+                consumo = entrada.next().charAt(0);
+
+                if ((consumo != 'A') && (consumo != 'B') && (consumo != 'C')) {
+                    continue;
+                }
+
+                break;
+            } catch (Exception e) {
+                System.out.println("Ingrese un caracter");
+                entrada.next();
+            }
+        }
+
+        return consumo;
+    }
+
+    private String ingresarProcedencia(Scanner entrada) {
+        int procedencia;
+
+        // El usuario ingresa consumo
+        while (true) {
+            try {
+                System.out.print("Ingrese 1 para Nacional\nIngrese 2 para Importado\n¿Qué procedencia desea escoger?: ");
+                procedencia = entrada.nextInt();
+
+                if (procedencia == 1) {
+                    return "Nacional";
+                } else if (procedencia == 2) {
+                    return "Importado";
+                } else {
+                    continue;
+                }
+            } catch (Exception e) {
+                System.out.println("Ingrese un numero entero");
+                entrada.next();
+            }
+        }
+    }
+
+    private void ingresarElectrodomesticos(Scanner entrada, int numeroElectrodomesticos) {
+        for (int i = 0; i < numeroElectrodomesticos; i++) {
+            // El usuario ingresa que electrodomestico quiere facturar
+            while (true) {
+                try {
+                    System.out.print("Ingrese 1 para electrodoméstico\nIngrese 2 para televisor\nIngrese 3 para nevera\n¿Qué desea agregar?: ");
+                    int opcion = entrada.nextInt();
+
+                    switch (opcion) {
+                        case 1: {
+                            char consumo = ingresarConsumo(entrada);
+                            String procedencia = ingresarProcedencia(entrada);
+                            ingresarElectrodomestico(consumo, procedencia, i);
+                            break;
+                        }
+                        case 2: {
+                            char consumo = ingresarConsumo(entrada);
+                            String procedencia = ingresarProcedencia(entrada);
+                            ingresarTelevisor(entrada, consumo, procedencia, i);
+                            break;
+                        }
+                        case 3: {
+                            char consumo = ingresarConsumo(entrada);
+                            String procedencia = ingresarProcedencia(entrada);
+                            ingresarNevera(entrada, consumo, procedencia, i);
+                            break;
+                        }
+                        default:
+                            System.out.println("Ingrese una opción en el rango de 1 a 3");
+                            i--;
+                            break;
+                    }
+                    break;
+                } catch (Exception e) {
+                    System.out.println("Favor ingresar un número entero");
+                    entrada.next();
+                }
+            }
+        }
+    }
+
+    private void ingresarElectrodomestico(char consumo, String procedencia, int i) {
+        Electrodomestico electrodomestico = new Electrodomestico(consumo, procedencia);
+        this.electrodomesticos.put(i, electrodomestico);
+    }
+
+    private void ingresarTelevisor(Scanner entrada, char consumo, String procedencia, int i) {
+        int tamano;
+        int entradaTDT;
+        boolean tieneTDT;
+
+        // Ingresar tamaño televisor
+        while (true) {
+            try {
+                System.out.print("Ingresar tamaño televisor (pulgadas): ");
+                tamano = entrada.nextInt();
+                break;
+            } catch (Exception e) {
+                System.out.println("Ingrese un número entero");
+                entrada.next();
+            }
+        }
+
+        // Ingresar si tiene TDT
+        while (true) {
+            try {
+                System.out.print("Ingrese 1 si tiene\nIngrese 2 si no tiene\n¿Tiene TDT?: ");
+                entradaTDT = entrada.nextInt();
+
+                if (entradaTDT == 1){
+                    tieneTDT = true;
+                    break;
+                } else if (entradaTDT == 2) {
+                    tieneTDT = false;
+                    break;
+                } else {
+                    continue;
+                }
+            } catch (Exception e) {
+                System.out.println("Ingrese un número entero");
+                entrada.next();
+            }
+        }
+
+        Electrodomestico televisor = new Televisor(consumo, procedencia, tamano, tieneTDT);
+        this.electrodomesticos.put(i, televisor);
+    }
+
+    private void ingresarNevera(Scanner entrada, char consumo, String procedencia, int i) {
+        int capacidad;
+
+        // Ingresar capacidad
+        while (true) {
+            try {
+                System.out.print("Ingresar capacidad nevera (litros): ");
+                capacidad = entrada.nextInt();
+                break;
+            } catch (Exception e) {
+                System.out.println("Ingrese un número entero");
+                entrada.next();
+            }
+        }
+
+        Electrodomestico nevera = new Nevera(consumo, procedencia, capacidad);
+        this.electrodomesticos.put(i, nevera);
+    }
+}
